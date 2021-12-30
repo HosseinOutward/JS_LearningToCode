b1e1ed7a6df39c754bea777910c32b03bc16a75a
HosseinOutward <42696675+HosseinOutward@users.noreply.github.com>
2021-12-30 15:55:08 +0330

    Add files via upload

--- Loop Cal/script.js
+++ Loop Cal/script.js
@@ -1,25 +1,30 @@
 alert("A car is going to start a race in a circular highway");
+
 //Input start
 var energy = prompt("How many kilometer(s) can the car go (it's energy)?\n(every 10000 J is 1 KM)");
 if (isNaN(energy)) {alert("Not a valid value.");} else {
   var distance = prompt("How long is the highway?");
   if (isNaN(distance)) {alert("Not a valid value.");} else {
 //Input end
+
 //Calculation start
       var runs = Math.abs(+energy /10000 / +distance)
-//If the car  never reaches the finish line:
+
+  //If the car  never reaches the finish line:
       if ( (runs < 1) && (runs > -1) ) {
         var fixerKM = runs * distance
         alert("The car will never reach the finish line\nbut it did go " + fixerKM + " KM of the highway's distance.");
       } else {
-//If the car loops for ever:
+
+  //If the car loops for ever:
           if (distance == 0) {
               for (var counter = 1 ; counter <= 3 ; counter++) {
                 alert("The car passed the finish line " + counter + " time(s).");
                 if (counter == 3) { alert("The car will loop till the end of time.\n(becuse its right on top of the finish line and you have to go 0 KM to reach it\n so your passing the finish line again and again til you move from tha point.)"); }
               }
           } else {
-//If the car passed the finish line more than once but stoped before reaching a last time:
+            
+  //If the car passed the finish line more than once but stoped before reaching a last time:
               var fixer = runs - runs.toFixed(0)
               if (fixer != 0) {
                 var fixer = runs - parseInt(runs)
@@ -30,15 +35,15 @@ if (isNaN(energy)) {alert("Not a valid value.");} else {
                   if (counter == runs) { alert("The car only looped for " + counter + "time(s)\nand continued " + fixer + " (" + fixerKM + " KM) but never reach the finish line after that."); }
                 }
               } else {
-//If the car was going normally:
+
+  //If the car was going normally:
                   for (counter = 1 ; counter <= runs ; counter++) {
                     alert("The car passed the finish line " + counter + " time(s).");
                     if (counter == runs) { alert("The car only looped for " + counter + " time(s)."); }
                 }
               }
-//Calculation end
         }
     }
-
+//Calculation end
       }
     }
--- Simple Calculator/Script.js
+++ Simple Calculator/Script.js
@@ -1,43 +1,76 @@
 //Input start
 var num1 = prompt("Num1");
-if ( ( num1 <= 0 ) || ( num1 > 0 ) ) {
+if ( !isNaN(num1) ) {
 
   var act = prompt("Action");
   if ( (act == "+") || (act == "-") || (act == "*") || (act == "/") ||
-  (act == "%") )
-   {
+  (act == "%") ) {
+
     var num2 = prompt("Num2");
-    if ( ( num1 <= 0 ) || ( num1 > 0 ) ) {
+    if ( !isNaN(num2) ) {
 //Input end
 
 //Calculator start
-switch ( act ) {
+
+function CalculationFN () {
+
+  switch ( act ) {
 
         case "+":
-         alert(num1 + num2);
-          break;
+           num1 = +num1 + +num2;
+            ReCalFN ();
+            break;
 
         case "-":
-         alert(num1 - num2);
-          break;
+           num1 = +num1 - +num2;
+            ReCalFN ();
+            break;
 
         case "*":
-         alert(num1 * num2);
-          break;
+           num1 = +num1 * +num2;
+            ReCalFN ();
+            break;
 
         case "/":
-         if ( ( act == "/" ) && ( num2 == 0) )
-         { alert("Error: Undifiend"); } else { alert(num1 / num2); }
-          break;
+           if ( ( act == "/" ) && ( num2 == 0) )
+           { alert("Error: Undifiend\nResult: Infinity"); } else {
+            num1 = +num1 / +num2; }
+             ReCalFN ();
+            break;
 
         case "%":
-         alert(num1 * num2 /100 );
-          break;
-      }
+           num1 = +num1 * +num2 /100;
+            ReCalFN ();
+            break;
+
+          }
+
+       }
+
+//.............................
+
+       function ReCalFN () {
+
+        act = prompt(" " + num1 + "\n New Action");
+        if ( (act == "+") || (act == "-") || (act == "*") || (act == "/") ||
+         (act == "%") ) {
+
+          num2 = prompt("AnyNum");
+          if ( !isNaN(num2) ) {
+
+     CalculationFN ();
+
+           } else { alert("Error: no valid action"); }
+         } else { alert("Error: no valid value"); }
+
+       }
+
 //Calculator end
+
+CalculationFN ();
 
 //Error start
     } else { alert("Error: no valid value"); }
-  } else { alert("Error: no valid function"); }
+  } else { alert("Error: no valid action"); }
 } else { alert("Error: no valid value"); }
 //Error end
